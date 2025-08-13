bc7c7f0999dee5b7b745a4fcc5eaaee713ee7585
Kumalo Ntando Topi <160849388+topi2005@users.noreply.github.com>
2025-08-13 17:00:23 +0200

    Update App.jsx
    imported the landing, signIn pages and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./Frontend/landing"; // ✅ New landing page component
+import SignInSignUp from "./Frontend/SignInSignUp"; // ✅ Auth form toggle componentimport { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
