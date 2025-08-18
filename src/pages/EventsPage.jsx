6cf6664535a5f8e1c92551e4aef18803c9ce3eef
Kutlwano Mokubetsi <2673228@students.wits.ac.za>
2025-08-19 01:10:15 +0200

    Fix deployment issues (Frontend)

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Navbar from "../Components/Navbar";
 import dayjs from "dayjs";
 
 const mockEvents = [
