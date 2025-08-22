686b6c05fa95e1d5ec7e38c081b78da65190347c
Ntobeko Mdakane <mdakanentobeko73@gmail.com>
2025-08-22 12:06:49 +0200

    Create EventCard component

---
+++ src/utils/eventColors.js
@@ -0,0 +1,22 @@
+export const eventColors = {
+  Wedding: {
+    bgColor: "bg-pink-200",
+    labelColor: "bg-pink-500",
+  },
+  Birthday: {
+    bgColor: "bg-blue-200",
+    labelColor: "bg-blue-500",
+  },
+  Conference: {
+    bgColor: "bg-yellow-200",
+    labelColor: "bg-yellow-700",
+  },
+  Meeting: {
+    bgColor: "bg-green-200",
+    labelColor: "bg-green-500",
+  },
+  Other: {
+    bgColor: "bg-gray-200",
+    labelColor: "bg-gray-500",
+  },
+};
