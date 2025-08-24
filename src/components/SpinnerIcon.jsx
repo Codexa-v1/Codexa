73bd86a8e82abb3dbef9bc6c70aed417e95c2b5f
marxian-disciple <2703800@students.wits.ac.za>
2025-08-24 08:16:25 +0200

    Made a branch just for .env

--- src/components/SpinnerIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Spinner() {
-  return (
-    <section className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30 z-50">
-      <section className="animate-spin rounded-full h-16 w-16 border-t-4 border-green-700 border-opacity-70"></section>
-    </section>
-  );
-}
--- src/mockuser.js
+++
@@ -1,6 +0,0 @@
-import profile from "./assets/profile.png";
-
-export const mockUser = {
-  name: "Speedy Chuck",
-  profilePic: profile, // or a placeholder URL
-};
