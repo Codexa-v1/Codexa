507558937ddfabc5760b9c361c052295ad9f9ffb
topi2005 <2666939@students.wits.ac.za>
2025-09-01 12:24:09 +0200

    More tests

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import path from 'path'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: "automatic", // enables the new JSX transform everywhere
+  })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',   // React Testing Library needs DOM
+    setupFiles: './src/setupTests.jsx', // Jest-DOM matchers, etc.
+    coverage: {
+      provider: 'v8',
+      reporter: ['lcov', 'text'], // required for Codecov + console output
+      reportsDirectory: "./coverage",
+      include: ['src/**/*.{js,jsx}'],
+      exclude: ['**/*.test.{js,jsx}', 'src/setupTests.js'],
+    },
+  },
 })
