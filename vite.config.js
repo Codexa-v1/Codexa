fbb22465cfc6329d6d316c2fc9f0026f13352316
marxian-disciple <2703800@students.wits.ac.za>
2025-09-01 09:45:22 +0200

    Separated vite.config from vitest.config

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
-import path from 'path'
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxRuntime: "automatic", // enables the new JSX transform everywhere
-  })],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',   // React Testing Library needs DOM
-    setupFiles: './src/setupTests.jsx', // Jest-DOM matchers, etc.
-    coverage: {
-      provider: 'v8',
-      reporter: ['lcov', 'text'], // required for Codecov + console output
-      reportsDirectory: "./coverage",
-      include: ['src/**/*.{js,jsx}'],
-      exclude: ['**/*.test.{js,jsx}', 'src/setupTests.js'],
     },
   },
 })
