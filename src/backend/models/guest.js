3e9ec1f804559378ef7c6bd86fbc5f8c38939a70
Kutlwano Mokubetsi <2673228@students.wits.ac.za>
2025-10-08 13:33:21 +0200

    Allow multiple email usage

--- src/backend/models/guest.js
+++ src/backend/models/guest.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const guest = new mongoose.Schema({
   // for id, just use the automatically created field in mongo
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true},
   phone: { type: String, required: true },
   eventId: { type: String, required: true },
   rsvpStatus: { type: String, enum: ['Pending', 'Accepted', 'Declined'], default: 'Pending', required: true },
